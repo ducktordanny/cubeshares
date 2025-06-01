fb5b7928cc05893090f22f343354051fa2da81ed
Daniel Lazar <lazardani0112@gmail.com>
2025-06-01 15:47:27 +0200

    feat(user): create user/me endpoint
    Get info about logged in user by using the new Authorization middleware.

--- backend/services/user/routes.go
+++ backend/services/user/routes.go
@@ -1,7 +1,11 @@
 package user
 
 import (
+	"net/http"
+
+	"github.com/ducktordanny/cubeit/backend/middlewares"
 	"github.com/ducktordanny/cubeit/backend/types"
+	"github.com/ducktordanny/cubeit/backend/utils"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,4 +17,16 @@ func NewHandler(store types.UserStore) *Handler {
 	return &Handler{store: store}
 }
 
-func (handler *Handler) RegisterRoutes(router *gin.RouterGroup) {}
+func (handler *Handler) RegisterRoutes(router *gin.RouterGroup) {
+	router.GET("user/me", middlewares.UserAuthSessionMiddleware(), handler.handleUserMe)
+}
+
+func (handler *Handler) handleUserMe(context *gin.Context) {
+	claims := utils.GetAuthClaims(context)
+	user, err := handler.store.GetUserById(claims.Sub)
+	if err != nil {
+		context.AbortWithStatusJSON(http.StatusNotFound, gin.H{"error": "Couldn't find authenticated user"})
+		return
+	}
+	context.IndentedJSON(http.StatusOK, user)
+}
