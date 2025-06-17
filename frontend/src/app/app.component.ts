e9371ff37bb9d1fd4f522e0e636efe2f8132acf8
Daniel Lazar <lazardani0112@gmail.com>
2025-06-17 10:34:58 +0200

    refact(frontend): set selector prefix & use better suffixes
    - Change `selector` prefix to "cubeit", makes more sense than just "app"
    - Use better file name suffixes for better distinctiveness

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  selector: 'cubeit-root',
+  templateUrl: './app.template.html',
+  styleUrl: './app.style.scss',
+  imports: [RouterOutlet],
 })
 export class AppComponent {}
