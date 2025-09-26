93a5c64b78329d4c6fcffaf16325ca420a8fb462
Daniel Lazar <lazardani0112@gmail.com>
2025-09-26 23:46:01 +0200

    feat(user-backend): Add new endpoint for updating bio.

--- backend/services/user/routes.go
+++ backend/services/user/routes.go
@@ -19,6 +19,7 @@ func NewHandler(store types.UserStore) *Handler {
 
 func (handler *Handler) RegisterRoutes(router *gin.RouterGroup) {
 	router.GET("user/me", middlewares.UserAuthSessionMiddleware(), handler.handleUserMe)
+	router.PUT("user/me/bio", middlewares.UserAuthSessionMiddleware(), handler.handleUserMeBio)
 }
 
 func (handler *Handler) handleUserMe(context *gin.Context) {
@@ -29,4 +30,19 @@ func (handler *Handler) handleUserMe(context *gin.Context) {
 		return
 	}
 	context.IndentedJSON(http.StatusOK, user)
+}
+
+func (handler *Handler) handleUserMeBio(context *gin.Context) {
+	claims := utils.GetAuthClaims(context)
+	var request types.UpdateUserBioRequestBody
+	if err := context.ShouldBindJSON(&request); err != nil {
+		context.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "Invalid request body"})
+		return
+	}
+	err := handler.store.UpdateUserBio(claims.Sub, request.Bio)
+	if err != nil {
+		context.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "Couldn't update user bio: " + err.Error()})
+		return
+	}
+	context.Status(http.StatusNoContent)
 }
